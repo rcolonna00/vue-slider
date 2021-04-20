8e592fa1f590bba420bbe9dc66b3004d7157a9ed
Rocco Colonna <colonna.rocco2000@gmail.com>
2021-04-20 18:53:11 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -2,10 +2,33 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-
+            currentActiveImage: 0,
+            carouselImages: [
+                'img/cascate.webp',
+                'img/campagna.jpg',
+                'img/fiori.jpeg',
+                'img/montagne.webp'
+            ],
         },
         methods: {
-            
+            nextPhoto() {
+                const nextImageIndex = this.currentActiveImage + 1;
+
+                if( nextImageIndex > this.carouselImages.length - 1 ) {
+                    this.currentActiveImage = 0;
+                } else {
+                    this.currentActiveImage = nextImageIndex;
+                }
+            },
+            prevPhoto() {
+                const prevImageIndex = this.currentActiveImage - 1;
+
+                if( prevImageIndex < 0 ) {
+                    this.currentActiveImage = this.carouselImages.length - 1;
+                } else {
+                    this.currentActiveImage = prevImageIndex;
+                }
+            }
         }
     }
 );
